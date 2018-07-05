3c12a1121e01a8a6fa80cf940d7f92fca07142b7
mike <mike@lexpredict.com>
2018-07-05 09:36:31 -0400

    update court2vec script

--- src/build_2vec_models.py
+++ src/build_2vec_models.py
@@ -1,6 +1,7 @@
 # Imports
 import gensim.models.word2vec
 import gensim.models.doc2vec
+import numpy.random
 import tarfile
 import tika.parser
 import ujson as json
@@ -9,8 +10,14 @@ import ujson as json
 from lexnlp.nlp.en.segments.sentences import get_sentence_list
 from lexnlp.nlp.en.tokens import get_token_list, get_stem_list
 
-# Setup input path
-tar_file_path = "../data/opinions/all.tar"
+# Setup input
+TAR_FILE_PATH = "../data/opinions/all.tar"
+
+# Proportion of each jurisdiction tar file to include in sample (selected randomly)
+CORPUS_PROPORTION = 0.1
+
+# Number of workers to use in gensim model building
+NUM_WORKERS = 1
 
 def process_text(text):
     stems = [s for s in get_stem_list(text, stopword=True, lowercase=True) if s.isalpha()]
@@ -22,13 +29,13 @@ if __name__ == "__main__":
     documents = []
 
     # open outer corpus file all.tar
-    with tarfile.open(tar_file_path, "r") as corpus_tar_file:
+    with tarfile.open(TAR_FILE_PATH, "r") as corpus_tar_file:
         corpus_member_list = corpus_tar_file.getmembers()
         corpus_num_members = len(corpus_member_list)
 
         # Iterate through all .tar.gz within
         for i, corpus_tar_member in enumerate(corpus_member_list):
-            print((tar_file_path, i, corpus_tar_member.name))
+            print((TAR_FILE_PATH, i, corpus_tar_member.name))
 
             # Open file object if tar.gz
             if not corpus_tar_member.name.lower().endswith(".tar.gz"):
@@ -38,32 +45,43 @@ if __name__ == "__main__":
                 # get file list within jurisdiction
                 court_member_list = court_tar_file.getmembers()
                 court_num_members = len(court_member_list)
+                court_sample_size = int(court_num_members * CORPUS_PROPORTION)
+                if court_sample_size > 0:
+                    court_sample_list = numpy.random.choice(court_member_list, court_sample_size)
+                else:
+                    continue
 
                 # iterate through opinions
-                for j, court_tar_member in enumerate(court_member_list):
-                    print((tar_file_path, i, corpus_tar_member.name,
+                for j, court_tar_member in enumerate(court_sample_list):
+                    print((TAR_FILE_PATH, i, corpus_tar_member.name,
                            court_tar_member.name, j))
 
                     # load json data
                     court_member_file = court_tar_file.extractfile(court_tar_member.name)
                     court_json_data = json.load(court_member_file)
 
-                    # select content source and get text content                
-                    if len(court_json_data['plain_text']) > 0:
-                        if "<p" in court_json_data['plain_text'].lower():
+                    # select content source and get text content
+                    try:
+                        if len(court_json_data['plain_text']) > 0:
+                            if "<p" in court_json_data['plain_text'].lower():
+                                html_content = "<html>{0}</html>".format(court_json_data['plain_text'])
+                                tika_response = tika.parser.from_buffer(html_content)
+                                text_content = tika_response['content']
+                            else:
+                                text_content = court_json_data['plain_text']
+                        elif len(court_json_data['html_with_citations']) > 0:
                             html_content = "<html>{0}</html>".format(court_json_data['html_with_citations'])
                             tika_response = tika.parser.from_buffer(html_content)
                             text_content = tika_response['content']
                         else:
-                            text_content = court_json_data['plain_text']
-                    elif len(court_json_data['html_with_citations']) > 0:
-                        html_content = "<html>{0}</html>".format(court_json_data['html_with_citations'])
-                        tika_response = tika.parser.from_buffer(html_content)
-                        text_content = tika_response['content']
-                    else:
-                        text_content = ''
+                            text_content = ''
+                    except Exception as e:
+                        print(("error in content extraction", e))
+                        continue
 
                     # skip if empty
+                    if text_content is None:
+                        continue
                     if len(text_content.strip()) == 0:
                         continue
 
@@ -84,7 +102,7 @@ if __name__ == "__main__":
     w2v_window_list = [5, 10, 20]
     for size in w2v_size_list:
         for window in w2v_window_list:
-            w2v_model_cbow = gensim.models.word2vec.Word2Vec(sentences, size=size, window=window, min_count=min_count, workers=1)
+            w2v_model_cbow = gensim.models.word2vec.Word2Vec(sentences, size=size, window=window, min_count=min_count, workers=NUM_WORKERS)
             w2v_model_cbow.save("../data/models/w2v_cbow_all_size{0}_window{1}".format(size, window))
             
     # doc2vec models
@@ -93,5 +111,7 @@ if __name__ == "__main__":
     d2v_window_list = [5, 10, 20]
     for size in d2v_size_list:
         for window in d2v_window_list:
-            d2v_model = gensim.models.doc2vec.Doc2Vec(documents, vector_size=size, window=window, min_count=min_count, workers=1)
+            d2v_model = gensim.models.doc2vec.Doc2Vec(documents, vector_size=size, window=window, min_count=min_count, workers=NUM_WORKERS)
             d2v_model.save("../data/models/d2v_all_size{0}_window{1}".format(size, window))
+
+    
